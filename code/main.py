9f7f7103ef22a5143d6b12cb0c1c9cd2f0fd0ad0
camillerange <79823780+camillerange@users.noreply.github.com>
2024-10-08 00:00:57 -0700

    Create main.py
    Create the main.py file for the Code directory.

---
+++ code/main.py
@@ -0,0 +1,11 @@
+'''
+The main file for programming for CS 516: Computers, Sound, and Music (Fall 2024)
+Author: Camille Range
+Sources/Credit will be stated for any code not solely from the author
+'''
+
+def main():
+  # main code
+
+if __name__ == "__main__":
+  main()
